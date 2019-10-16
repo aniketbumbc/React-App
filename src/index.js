fee07ed70095f63da9e706459916c16aa617f711
aniket.bhavsar <aniket.bhavsar@accionlabs.com>
2019-10-16 12:20:50 +0530

    react first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class App extends Component {
+      render() {
+            const name = 'React App';
+            const heading = <h3> Test {name} </h3> // create variable with HTML style
+            return (
+                  <div className="App"> 
+                        <h4> Hello!!  {name}</h4>
+                        {heading}
+                  </div>
+                  )
+      }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
