6cb85f55db2f10762c3bcaeb6bf8a5a7d85c3274
aniket.bhavsar <aniket.bhavsar@accionlabs.com>
2019-10-23 13:30:25 +0530

    Updates

---
+++ precache-manifest.f23d5b6c7b8cb242526729b4a1ddd5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1651c6cf64235ee0e337f22c5979d9d",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "12a61a206259def8e809",
+    "url": "/React-App/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7e5724a52a2fd22aceb0",
+    "url": "/React-App/static/css/main.4d210bbb.chunk.css"
+  },
+  {
+    "revision": "12a61a206259def8e809",
+    "url": "/React-App/static/js/2.338bf0cb.chunk.js"
+  },
+  {
+    "revision": "7e5724a52a2fd22aceb0",
+    "url": "/React-App/static/js/main.97075744.chunk.js"
+  },
+  {
+    "revision": "48fc0dae3e8232be7387",
+    "url": "/React-App/static/js/runtime-main.a3abca43.js"
+  }
+]);
